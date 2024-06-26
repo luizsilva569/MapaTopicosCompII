ad390aa6bdaab30838e069294856e63d07b45800
luizsilva569 <79003072+luizsilva569@users.noreply.github.com>
2024-06-25 22:28:15 -0400

    terceiro  commit

--- mapa/src/App.java
+++ mapa/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        String texto;
+
+        while (true) {
+            System.out.print("Digite uma palavra (ou 'sair' para encerrar): ");
+            texto = scan.next();
+    
+            // Verifica se o usuário digitou sair
+            if (texto.equalsIgnoreCase("sair")) {
+                System.out.println("Sistema encerrado.");
+                break; 
+            }
+            System.out.println("Você digitou: " + texto);
+        }
+        scan.close();
     }
 }
